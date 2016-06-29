a6248626c2c9a90f0a2a16c75912bbfde3ec10be
nateblodgett <nate.blodgett@t-mobile.com>
2016-06-29 14:44:50 -0700

    Update constants.js

--- src/main/webapp/vxml/constants.js
+++ src/main/webapp/vxml/constants.js
@@ -1,7 +1,7 @@
 var consts             = new Object();
 
 
-consts.var1       = 'a5677';
+consts.var1       = 'a56787';
 consts.var2       = 'bb';
 consts.var3       = 'ccc5';
 consts.var4       = 'dd';
